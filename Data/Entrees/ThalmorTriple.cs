975efa9dee2d485fad526c7e8d7ae8aa2640360a
Mason Wittman <mawittma@ksu.edu>
2020-09-06 20:00:13 -0500

    All tests done

--- Data/Entrees/ThalmorTriple.cs
+++ Data/Entrees/ThalmorTriple.cs
@@ -122,8 +122,16 @@ namespace BleakwindBuffet.Data.Entrees
             get
             {
                 List<string> instructions = new List<string>();
-                if (!Bacon) instructions.Add("Hold Bacon, ");
-                if (!Egg) instructions.Add("Hold Egg, ");
+                if (!Bun) instructions.Add("Hold bun");
+                if (!Ketchup) instructions.Add("Hold ketchup");
+                if (!Mustard) instructions.Add("Hold mustard");
+                if (!Pickle) instructions.Add("Hold pickle");
+                if (!Cheese) instructions.Add("Hold cheese");
+                if (!Tomato) instructions.Add("Hold tomato");
+                if (!Lettuce) instructions.Add("Hold lettuce");
+                if (!Mayo) instructions.Add("Hold mayo");
+                if (!Bacon) instructions.Add("Hold bacon");
+                if (!Egg) instructions.Add("Hold egg");
                 return instructions;
             }
         }
